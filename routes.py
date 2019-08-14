69e612bc265f0205b79b48b816de9b28147e22dd
Shamil <vagabov.sha@phystech.edu>
2019-08-14 15:18:21 +0300

    all request handlers added, but not all validation cases implemented and some algorithms can be improved

--- routes.py
+++ routes.py
@@ -2,10 +2,14 @@ import json
 import uuid
 from flask import request, jsonify
 from app import app, db
-from utils import json_response, JSON_MIME_TYPE
+from utils import json_response, JSON_MIME_TYPE, calculate_age, calculate_age_arr
 from validation import validate_date_and_citizens_id, citizens_schema, patch_req_schema
+from sqlalchemy.sql.expression import func
 from jsonschema import validate, ValidationError, SchemaError
 from models import Citizen, CitizenSchema
+from numpy import zeros, percentile
+from datetime import datetime
+from sqlalchemy.orm import Session
 
 
 @app.route("/imports", methods=["POST"])
@@ -18,8 +22,9 @@ def imports():
 
     try:
         validate(data, citizens_schema)
-    except ValidationError:
+    except ValidationError as e:
         error = json.dumps({'error': 'JSON validation was not successfull'})
+        print(e.message)
         return json_response(error, 400)
     except SchemaError:
         error = json.dumps({'error': 'Invalid JSON Schema'})
@@ -37,13 +42,14 @@ def imports():
     import_uuid = str(uuid.uuid4())
 
     # create object in database
+    print(data['citizens'])
     for citizen in data["citizens"]:
-        new_citizen = Citizen(citizen_id=citizen["citizen_id"], town=citizen["town"], street=citizen["street"],
-                              building=citizen["building"], apartment=citizen["apartment"], name=citizen["name"],
-                              birth_date=citizen["birth_date"], gender=citizen["gender"], relatives=citizen["relatives"],
+        new_citizen = Citizen(citizen_id=citizen.get("citizen_id"), town=citizen.get("town"), street=citizen.get("street"),
+                              building=citizen.get("building"), apartment=citizen.get("apartment"), name=citizen.get("name"),
+                              birth_date=citizen.get("birth_date"), gender=citizen.get("gender"), relatives=citizen.get("relatives"),
                               import_id=import_uuid)
         db.session.add(new_citizen)
-        db.session.commit()
+    db.session.commit()
 
     import_id = {
         "data": {
@@ -75,7 +81,7 @@ def modify(import_id, citizen_id):
         return json_response(error, 400)
 
     citizens = Citizen.query.filter_by(import_id=import_id).all()
-    citizen = Citizen.query.filter_by(import_id=import_id, citizen_id=citizen_id).first()
+    updated_citizen = Citizen.query.filter_by(import_id=import_id, citizen_id=citizen_id).first()
     citizens_ids = []
 
     for elem in citizens:
@@ -84,11 +90,11 @@ def modify(import_id, citizen_id):
         error = json.dumps({'error': 'Citizens for certain import_id do not have citizen_id from input JSON'})
         return json_response(error, 400)
 
-    former_relatives = citizen.relatives
+    former_relatives = updated_citizen.relatives
     for key, value in data.items():
-        setattr(citizen, key, value)
+        setattr(updated_citizen, key, value)
     db.session.commit()
-    new_relatives = citizen.relatives
+    new_relatives = updated_citizen.relatives
 
     missing_relatives = [item for item in former_relatives if item not in new_relatives]
     added_relatives = [item for item in new_relatives if item not in former_relatives]
@@ -110,7 +116,7 @@ def modify(import_id, citizen_id):
             db.session.commit()
 
     citizen_schema = CitizenSchema()
-    output = citizen_schema.dump(citizen).data
+    output = citizen_schema.dump(updated_citizen).data
     return json_response(jsonify({'data': output}), 200)
 
 
@@ -121,6 +127,103 @@ def citizens(import_id):
     output = citizen_schema.dump(certain_citizens).data
     return jsonify({'data': output})
 
+
+@app.route("/imports/<string:import_id>/citizens/birthdays", methods=["GET"])
+def birthdays(import_id):
+    citizens = Citizen.query.filter_by(import_id=import_id).all()
+    birthdays_dict = {}
+
+    # for citizen in citizens:
+    #     citizen_id = citizen.citizen_id
+    #     # amount of presents certain citizen should buy(index of array = num of month)
+    #     birthdays_dict[citizen_id] = zeros(12)
+    #     for relative_id in citizen.relatives:
+    #         relative_info = (list(filter(lambda person: person.citizen_id == relative_id, citizens)))[0]
+    #         print(relative_info)
+    #         relative_bthday = datetime.strptime(relative_info.birth_date, "%d.%m.%Y").month
+    #         birthdays_dict[citizen_id][relative_bthday - 1] += 1
+
+    for citizen in citizens:
+        citizen_id = citizen.citizen_id
+        # amount of presents certain citizen should buy(index of array = num of month)
+        birthdays_dict[citizen_id] = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0}
+        for relative_id in citizen.relatives:
+            relative_info = (list(filter(lambda person: person.citizen_id == relative_id, citizens)))[0]
+            relative_bthday = datetime.strptime(relative_info.birth_date, "%d.%m.%Y").month
+            birthdays_dict[citizen_id][(relative_bthday - 1)] += 1
+
+    response = {
+        'data': {
+            '1': [],
+            '2': [],
+            '3': [],
+            '4': [],
+            '5': [],
+            '6': [],
+            '7': [],
+            '8': [],
+            '9': [],
+            '10': [],
+            '11': [],
+            '12': []
+        }
+    }
+
+    # for cit_id, presents_arr in birthdays_dict.items():
+    #     for index, val in enumerate(presents_arr):
+    #         if val != 0:
+    #             response['data'][str(index + 1)].append({'citizen_id': f'{cit_id}', 'presents': val})
+    #         continue
+
+    for cit_id, presents_arr in birthdays_dict.items():
+        for key, val in presents_arr.items():
+            if val != 0:
+                response['data'][str(key + 1)].append({'citizen_id': f'{cit_id}', 'presents': val})
+            continue
+
+    return json_response(json.dumps(response), 200)
+
+
+@app.route("/imports/<string:import_id>/towns/stat/percentile/age", methods=["GET"])
+def count_percentile(import_id):
+    # Extract data from db in a structure like : [('City_1', ['brthday_1 for city', '..', ..], ('City_2', [....]), etc.]
+    brth_days_grouped_by_town = db.session.query(Citizen.town, func.array_agg(Citizen.birth_date))   \
+             .filter_by(import_id=import_id)   \
+             .group_by(Citizen.town).all()
+    test_dict = dict(brth_days_grouped_by_town)
+    # result_dict = {}
+    response = {
+        'data': []
+    }
+
+    # for local_info in brth_days_grouped_by_town:
+    #     ages = []
+    #     for brth_day in local_info[1]:
+    #         ages.append(calculate_age(datetime.strptime(brth_day, '%d.%m.%Y')))
+    #     result_dict[local_info[0]] = ages
+    # for key, value in result_dict.items():
+    #     p50 = round(percentile(value, 50, interpolation='linear'), 2)
+    #     p75 = round(percentile(value, 75, interpolation='linear'), 2)
+    #     p99 = round(percentile(value, 99, interpolation='linear'), 2)
+    #     response['data'].append({'town': f'{key}', 'p50': p50, 'p75': p75, 'p99': p99})
+
+    # for town_info in brth_days_grouped_by_town:
+    #     ages = []
+    #     for brth_day in town_info[1]:
+    #         ages.append(calculate_age(datetime.strptime(brth_day, '%d.%m.%Y')))
+    #     p50 = round(percentile(ages, 50, interpolation='linear'), 2)
+    #     p75 = round(percentile(ages, 75, interpolation='linear'), 2)
+    #     p99 = round(percentile(ages, 99, interpolation='linear'), 2)
+    #     response['data'].append({'town': f'{town_info[0]}', 'p50': p50, 'p75': p75, 'p99': p99})
+
+    for key, value in test_dict.items():
+        arr = calculate_age_arr(value)
+        p50 = round(percentile(arr, 50, interpolation='linear'), 2)
+        p75 = round(percentile(arr, 75, interpolation='linear'), 2)
+        p99 = round(percentile(arr, 99, interpolation='linear'), 2)
+        response['data'].append({'town': f'{key}', 'p50': p50, 'p75': p75, 'p99': p99})
+
+    return json_response(json.dumps(response), 200)
 
 
 
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from flask import make_response
+from datetime import datetime
 
 JSON_MIME_TYPE = 'application/json'
 
@@ -9,4 +10,20 @@ def json_response(data='', status=201, headers=None):
         headers['Content-Type'] = JSON_MIME_TYPE
 
     return make_response(data, status, headers)
+
+
+# pass birth_day in format of datetime to calculate age
+def calculate_age(brth_day):
+    today = datetime.utcnow()
+    return today.year - brth_day.year - ((today.month, today.day) < (brth_day.month, brth_day.day))
+
+
+def calculate_age_arr(brth_days):
+    today = datetime.utcnow()
+    new_arr = []
+    for index, value in enumerate(brth_days):
+        datetime_format = datetime.strptime(value, '%d.%m.%Y')
+        brth_days[index] = today.year - datetime_format.year - ((today.month, today.day) < (datetime_format.month, datetime_format.day))
+        # new_arr.append(today.year - elem.year - ((today.month, today.day) < (elem.month, elem.day)))
+    return brth_days
 
